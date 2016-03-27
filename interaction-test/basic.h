2251c3b9bcdeb7008b4efaae656f6ace805f9ac1
Dan Haman <uef_mailbox@yahoo.co.uk>
2016-03-27 19:56:05 +0300

    + basic improved, on/off extracted

--- interaction-test/basic.h
+++ interaction-test/basic.h
@@ -1,5 +1,18 @@
+#ifndef _BASIC_H_
+#define _BASIC_H_
+
 #include "avr/io.h"
+
+#ifndef ON 
+#define ON true 
+#endif
+
+#ifndef OFF 
+#define OFF false
+#endif
 
 void write_pin(volatile uint8_t *port, uint8_t pin, uint8_t value);
 
 uint8_t read_pin(uint8_t port, uint8_t pin);
+
+#endif
